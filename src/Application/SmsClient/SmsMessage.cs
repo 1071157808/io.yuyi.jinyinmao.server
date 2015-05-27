eaead8145c2de769b4b59db038dab45acc9f04b2
Siqi Lu <lu.siqi@outlook.com>
2015-05-27 20:36:34 +0800

    201505272036

--- src/Application/SmsClient/SmsMessage.cs
+++ src/Application/SmsClient/SmsMessage.cs
@@ -1,44 +1,48 @@
-﻿// ***********************************************************************
+// ***********************************************************************
 // Project          : io.yuyi.jinyinmao.server
 // Author           : Siqi Lu
-// Created          : 2015-04-17  10:12 AM
+// Created          : 2015-04-19  5:34 PM
 //
 // Last Modified By : Siqi Lu
-// Last Modified On : 2015-04-19  12:54 PM
+// Last Modified On : 2015-05-27  8:27 PM
 // ***********************************************************************
-// <copyright file="SmsMessage.cs" company="Shanghai Yuyi">
-//     Copyright ©  2012-2015 Shanghai Yuyi. All rights reserved.
+// <copyright file="SmsMessage.cs" company="Shanghai Yuyi Mdt InfoTech Ltd.">
+//     Copyright ©  2012-2015 Shanghai Yuyi Mdt InfoTech Ltd. All rights reserved.
 // </copyright>
 // ***********************************************************************
 
+using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json;
 
 namespace SmsClient
 {
+    /// <summary>
+    ///     SmsMessage.
+    /// </summary>
     public class SmsMessage
     {
         /// <summary>
         ///     手机号，多个号码以,分隔，这里不会验证手机号的格式是否正确
         /// </summary>
-        [JsonProperty("cellphones")]
+        [JsonProperty("cellphones"), SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
         public string Cellphones { get; set; }
 
         /// <summary>
         ///     短信通道
         /// </summary>
-        [JsonProperty("channel")]
+        [JsonProperty("channel"), SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
         public string Channel { get; set; }
 
         /// <summary>
         ///     短信内容
         /// </summary>
-        [JsonProperty("message")]
+        [JsonProperty("message"), SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
         public string Message { get; set; }
 
         /// <summary>
         ///     短信签名
         /// </summary>
-        [JsonProperty("signature")]
+        [JsonProperty("signature"), SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
         public string Signature { get; set; }
     }
 }
