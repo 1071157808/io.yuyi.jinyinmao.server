b428a917a45f2a76bd04707808216c10b07ba811
= <wang.bing@jym-t20150625a.ad.jinyinmao.com.cn>
2015-07-08 16:51:16 +0800

    update SagasTransfer 20150708

---
+++ src/Application/SagasTransfer/FileHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagasTransfer
+{
+    public static class FileHelper
+    {
+        public static void WriteTo(string baseDir,string msg)
+        {
+            string dir = Path.Combine(baseDir, "log");
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            string fileName = $"{DateTime.UtcNow.AddHours(8).ToString("yyyyMMdd")}.txt";
+            using (StreamWriter sw = new StreamWriter(File.Open(Path.Combine(dir, fileName), FileMode.Append)))
+            {
+                sw.WriteLine("--------------------------------Error--------------------------------");
+                sw.WriteLine($"Time:{DateTime.UtcNow.AddHours(8).ToLongDateString()} ");
+                sw.WriteLine(msg);
+                sw.WriteLine("--------------------------------Error--------------------------------");
+            }
+        }
+    }
+}
