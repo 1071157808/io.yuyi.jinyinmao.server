5c24561cba9e92f6c9032c50a09e67dd49c7fe46
KingWang1 <1872500341@qq.com>
2015-08-18 11:14:28 +0800

    add short link project

---
+++ src/Api/Yuyi.Jinyinmao.Api.Link/App_Start/WebApiConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Yuyi.Jinyinmao.Api.Link
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+        }
+    }
+}
